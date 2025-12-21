676d4e0c3b46b272a26ad390ea1812325c2790e0
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-12-21 16:06:26 +0100

    add AppHeader.vue Komponent

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <input type="text" placeholder="Suche...">
+    </header>
+</template>
+
+<script>
+
+    export default { 
+        name: 'AppHeader',
+    }
+
+</script>
+
+<style scoped>
+header {
+    background-color: #F4BC39;
+    height: 120px;
+    padding: 16px;
+    display: flex;
+    align-items: flex-end;
+}
+
+input {
+    border: 0;
+    border-radius: 8px;
+    width: 100%;
+    height: 30px;
+}
+</style>
